eee4757580a877e078afaacf5935294603ee2e42
Jeremy Sabina <Jeremy_Sabina@yahoo.com>
2024-02-19 22:02:53 -0500

    added more game logic

--- space-invaders/app.js
+++ space-invaders/app.js
@@ -2,10 +2,13 @@ const grid = document.querySelector('.grid');
 const resultDisplay = document.querySelector('.results');
 
 const width = 15
+const aliensRemoved = []
+let currentShooterIndex = 202
 
 
 for(let i = 0; i < width * width; i++) {
     const square = document.createElement('div')
+    square.id = i
     grid.appendChild(square)
 }
 
@@ -19,10 +22,32 @@ const alienInvaders = [
     30,31,32,33,34,35,36,37,38,39
 ]
 
+
+
 function draw() {
     for(let i = 0; i < alienInvaders.length; i++) {
+        if (!aliensRemoved.includes(i)) {
         squares[alienInvaders[i]].classList.add('invader')
+
+        }
     }
 }
 
 draw()
+
+squares[currentShooterIndex].classList.add('shooter')
+
+function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove('shooter')
+    switch(e.key) {
+        case 'ArrowLeft':
+            if (currentShooterIndex % width !==0) currentShooterIndex -=1
+            break
+        case 'ArrowRight':
+            if (currentShooterIndex % width < width - 1) currentShooterIndex +=1
+            break
+    }
+    squares[currentShooterIndex].classList.add('shooter')
+}
+
+document.addEventListener('keydown', moveShooter)
