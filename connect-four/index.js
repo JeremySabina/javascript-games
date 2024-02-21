2cad896818bd7a9df66337bc726723fbaf55e7fc
Jeremy Sabina <jeremysabina@gmail.com>
2024-02-21 02:00:38 -0500

    Started Connect four video

---
+++ connect-four/index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const results = document.querySelector('#result')
+    const displaycurrentPlayer = document.querySelector('#current-player')
+    let currentPlayer = 1
+  
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].onclick = () => {
+        }
+    }
+})
